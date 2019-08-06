a0114649c27739fdd4a54bd1892b40317c7e6b44
Abhishek Purohit <apurohit@cloudera.com>
2019-08-06 14:51:03 -0700

    CDPCP-496 - Initial Code changes

--- freeipa/src/main/java/com/sequenceiq/freeipa/service/freeipa/user/model/UmsState.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/service/freeipa/user/model/UmsState.java
@@ -15,17 +15,27 @@ import com.cloudera.thunderhead.service.usermanagement.UserManagementProto.User;
 public class UmsState {
     private Map<String, Group> groupMap;
 
+    // Regular users
     private Map<String, User> userMap;
 
     private Map<String, GetRightsResponse> userRightsMap;
+
+    // Admin Users for an environment.
+    private Map<String, User> adminUserMap;
+    private Map<String, GetRightsResponse> adminUserRightsMap = new HashMap<>();
+
+    private Map<String, MachineUser> adminMachineUserMap = new HashMap<>();
 
     private Map<String, MachineUser> machineUserMap;
 
     private Map<String, GetRightsResponse> machineUserRightsMap;
 
-    public UmsState(Map<String, Group> groupMap, Map<String, User> userMap, Map<String, GetRightsResponse> userRightsMap,
-            Map<String, MachineUser> machineUserMap, Map<String, GetRightsResponse> machineUserRightsMap) {
+    public UmsState(Map<String, Group> groupMap, Map<String, User> adminUserMap, Map<String, GetRightsResponse> adminUserRightsMap, Map<String, MachineUser> adminMachineUserMap, Map<String, User> userMap, Map<String, GetRightsResponse> userRightsMap,
+                    Map<String, MachineUser> machineUserMap, Map<String, GetRightsResponse> machineUserRightsMap) {
         this.groupMap = requireNonNull(groupMap);
+        this.adminUserMap = requireNonNull(adminUserMap);
+        this.adminUserRightsMap = requireNonNull(adminUserRightsMap);
+        this.adminMachineUserMap = adminMachineUserMap;
         this.userMap = requireNonNull(userMap);
         this.userRightsMap = requireNonNull(userRightsMap);
         this.machineUserMap = requireNonNull(machineUserMap);
@@ -36,6 +46,8 @@ public class UmsState {
         UsersState.Builder builder = new UsersState.Builder();
 
         Map<String, com.sequenceiq.freeipa.api.v1.freeipa.user.model.Group> crnToGroup = new HashMap<>(groupMap.size());
+
+        // TODO: No need to sync all the groups - remove below code
         groupMap.entrySet()
                 .forEach(e -> {
                     com.sequenceiq.freeipa.api.v1.freeipa.user.model.Group group = umsGroupToGroup(e.getValue());
@@ -43,26 +55,50 @@ public class UmsState {
                     builder.addGroup(group);
                 });
 
-        // TODO filter users by environment rights
+        // TODO filter users by environment rights - Note: Now UmsState is for the envCrn.
+
+        // Regular Users
         userMap.entrySet()
                 .forEach(e -> {
                     com.sequenceiq.freeipa.api.v1.freeipa.user.model.User user = umsUserToUser(e.getValue());
                     builder.addUser(user);
-                    // TODO remove `admins` membership once the group mapping is figured out (CB-2003, DISTX-95)
-                    builder.addMemberToGroup("admins", user.getName());
-                    userRightsMap.get(e.getKey()).getGroupCrnList()
-                            .forEach(crn -> {
-                                builder.addMemberToGroup(crnToGroup.get(crn).getName(), user.getName());
-                            });
+//                    userRightsMap.get(e.getKey()).getGroupCrnList()
+//                            .forEach(crn -> {
+//                                builder.addMemberToGroup(crnToGroup.get(crn).getName(), user.getName());
+//                            });
                 });
+
+
+        // env Admin Users
+        adminUserMap.entrySet()
+            .forEach(adminUser -> {
+                // Admin users are also regular users but must be added to specific group
+                com.sequenceiq.freeipa.api.v1.freeipa.user.model.User user = umsUserToUser(adminUser.getValue());
+                builder.addUser(user);
+                // TODO remove `admins` membership once the group mapping is figured out (CB-2003, DISTX-95)
+                // TODO: change admins group to cpd_env_admin_<#env_name>, need to parse env crn and pass that group value.
+                builder.addMemberToGroup("admins", user.getName());
+            });
 
         // TODO filter machine users by environment rights
         machineUserMap.entrySet()
                 .forEach(e -> {
                     com.sequenceiq.freeipa.api.v1.freeipa.user.model.User user = umsMachineUserToUser(e.getValue());
                     builder.addUser(user);
-                    machineUserRightsMap.get(e.getKey()).getGroupCrnList()
-                            .forEach(crn -> builder.addMemberToGroup(crnToGroup.get(crn).getName(), user.getName()));
+//                    machineUserRightsMap.get(e.getKey()).getGroupCrnList()
+//                            .forEach(crn -> builder.addMemberToGroup(crnToGroup.get(crn).getName(), user.getName()));
+                });
+
+        // Machine Admin Users
+        adminMachineUserMap.entrySet()
+                .forEach(e -> {
+                    com.sequenceiq.freeipa.api.v1.freeipa.user.model.User user = umsMachineUserToUser(e.getValue());
+                    builder.addUser(user);
+                    // TODO remove `admins` membership once the group mapping is figured out (CB-2003, DISTX-95)
+                    // TODO: change admins group to cpd_env_admin_<#env_name>, need to parse env crn and pass that group value.
+                    builder.addMemberToGroup("admins", user.getName());
+//                    machineUserRightsMap.get(e.getKey()).getGroupCrnList()
+//                            .forEach(crn -> builder.addMemberToGroup(crnToGroup.get(crn).getName(), user.getName()));
                 });
 
         return builder.build();
@@ -112,6 +148,12 @@ public class UmsState {
 
         private Map<String, GetRightsResponse> userRightsMap = new HashMap<>();
 
+        private Map<String, User> adminUserMap = new HashMap<>();
+
+        private Map<String, GetRightsResponse> adminUserRightsMap = new HashMap<>();
+
+        private Map<String, MachineUser> adminMachineUserMap = new HashMap<>();
+
         private Map<String, MachineUser> machineUserMap = new HashMap<>();
 
         private Map<String, GetRightsResponse> machineUserRightsMap = new HashMap<>();
@@ -126,6 +168,18 @@ public class UmsState {
             userRightsMap.put(userCrn, rights);
         }
 
+        public void addAdminUser(User user) {
+            String userCrn = user.getCrn();
+            adminUserMap.put(userCrn, user);
+            //adminUserRightsMap.put(userCrn, rights);
+        }
+
+        public void addAdminMachineUser(MachineUser machineAdminuser) {
+            // Machine User can be a power user also.
+            String userCrn = machineAdminuser.getCrn();
+            adminMachineUserMap.put(userCrn, machineAdminuser);
+        }
+
         public void addMachineUser(MachineUser machineUser, GetRightsResponse rights) {
             String machineUserCrn = machineUser.getCrn();
             machineUserMap.put(machineUserCrn, machineUser);
@@ -133,7 +187,7 @@ public class UmsState {
         }
 
         public UmsState build() {
-            return new UmsState(groupMap, userMap, userRightsMap, machineUserMap, machineUserRightsMap);
+            return new UmsState(groupMap, adminUserMap, adminUserRightsMap, adminMachineUserMap, userMap, userRightsMap, machineUserMap, machineUserRightsMap);
         }
     }
 }
