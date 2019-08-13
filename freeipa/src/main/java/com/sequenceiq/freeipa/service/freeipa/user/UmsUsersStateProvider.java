edcf907ae68d5a074ef17cfc1674001501002e65
David Han <dhan@cloudera.com>
2019-08-13 15:36:08 -0500

    CDPCP-548. Calculate user and group state from UMS
    CDPCP-496 - added env admin and users
    
    CDPCP-496 - checkstyle fix
    
    CDPCP-496 - moved request id up in stack
    
    CDPCP-496 - added full exception
    
    Minor - requestId, cleanup
    
    CDPCP-496 - added group to be synced

--- freeipa/src/main/java/com/sequenceiq/freeipa/service/freeipa/user/UmsUsersStateProvider.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/service/freeipa/user/UmsUsersStateProvider.java
@@ -1,18 +1,26 @@
 package com.sequenceiq.freeipa.service.freeipa.user;
 
+import com.cloudera.thunderhead.service.usermanagement.UserManagementProto.GetRightsResponse;
 import com.cloudera.thunderhead.service.usermanagement.UserManagementProto.Group;
 import com.cloudera.thunderhead.service.usermanagement.UserManagementProto.MachineUser;
+import com.cloudera.thunderhead.service.usermanagement.UserManagementProto.ResourceRoleAssignment;
+import com.cloudera.thunderhead.service.usermanagement.UserManagementProto.RoleAssignment;
 import com.cloudera.thunderhead.service.usermanagement.UserManagementProto.User;
 import com.sequenceiq.cloudbreak.auth.altus.GrpcUmsClient;
 import com.sequenceiq.cloudbreak.auth.altus.exception.UmsOperationException;
-import com.sequenceiq.freeipa.service.freeipa.user.model.UmsState;
+import com.sequenceiq.cloudbreak.logger.LoggerContextKey;
+import com.sequenceiq.cloudbreak.logger.MDCBuilder;
+import com.sequenceiq.freeipa.service.freeipa.user.model.FmsGroup;
+import com.sequenceiq.freeipa.service.freeipa.user.model.FmsUser;
+import com.sequenceiq.freeipa.service.freeipa.user.model.UsersState;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -24,90 +32,145 @@ import org.springframework.stereotype.Service;
 public class UmsUsersStateProvider {
     private static final Logger LOGGER = LoggerFactory.getLogger(UmsUsersStateProvider.class);
 
+    // TODO refactor ThreadBasedRequestIdProvider and replace this ThreadLocal
+    private static ThreadLocal<String> requestId = new ThreadLocal<>();
+
     @Inject
     private GrpcUmsClient umsClient;
 
     private final String environmentWrite = "environments/write";
 
-    public Map<String, UmsState> getEnvToUmsStateMap(String accountId, String actorCrn, Set<String> environmentsFilter,
-                Set<String> userCrns, Set<String> machineUserCrns) {
-        if (environmentsFilter == null || environmentsFilter.size() == 0) {
-            LOGGER.error("Environment Filter argument is null of empty");
-            throw new RuntimeException("Environment Filter argument is null of empty");
-        }
-
+    public Map<String, UsersState> getEnvToUmsUsersStateMap(String accountId, String actorCrn, Set<String> environmentsFilter,
+                                                            Set<String> userCrns, Set<String> machineUserCrns, Optional<String> requestIdOptional) {
         try {
-            List<User> users = userCrns.isEmpty() ? umsClient.listAllUsers(actorCrn, accountId, Optional.empty())
-                    : umsClient.listUsers(actorCrn, accountId, List.copyOf(userCrns), Optional.empty());
-            List<MachineUser> machineUsers = machineUserCrns.isEmpty() ? umsClient.listAllMachineUsers(actorCrn, accountId, Optional.empty())
-                    : umsClient.listMachineUsers(actorCrn, accountId, List.copyOf(machineUserCrns), Optional.empty());
+            if (!requestIdOptional.isPresent()) {
+                requestIdOptional = Optional.ofNullable(MDCBuilder.getMdcContextMap().get(LoggerContextKey.REQUEST_ID.toString()));
+            }
+            requestId.set(requestIdOptional.orElse(UUID.randomUUID().toString()));
+            LOGGER.debug("Getting UMS state for environments {} with requestId {}", environmentsFilter, requestIdOptional);
 
-            return getEnvToUmsStateMap(accountId, actorCrn, environmentsFilter, users, machineUsers);
+            Map<String, UsersState> envUsersStateMap = new HashMap<>();
 
+
+            List<User> users = userCrns.isEmpty() ? umsClient.listAllUsers(actorCrn, accountId, requestIdOptional)
+                    : umsClient.listUsers(actorCrn, accountId, List.copyOf(userCrns), requestIdOptional);
+            List<MachineUser> machineUsers = machineUserCrns.isEmpty() ? umsClient.listAllMachineUsers(actorCrn, accountId, requestIdOptional)
+                    : umsClient.listMachineUsers(actorCrn, accountId, List.copyOf(machineUserCrns), requestIdOptional);
+
+            Map<String, FmsGroup> crnToFmsGroup = umsClient.listGroups(actorCrn, accountId, List.of(), requestIdOptional).stream()
+                    .collect(Collectors.toMap(Group::getCrn, this::umsGroupToGroup));
+
+            environmentsFilter.stream().forEach(envCRN -> {
+                UsersState.Builder userStateBuilder = new UsersState.Builder();
+
+                users.stream().forEach(u -> {
+                    FmsUser fmsUser = umsUserToUser(u);
+                    handleUser(userStateBuilder, crnToFmsGroup, actorCrn, u.getCrn(), fmsUser, envCRN);
+                });
+
+                machineUsers.stream().forEach(mu -> {
+                    FmsUser fmsUser = umsMachineUserToUser(mu);
+                    handleUser(userStateBuilder, crnToFmsGroup, actorCrn, mu.getCrn(), fmsUser, envCRN);
+                });
+
+                envUsersStateMap.put(envCRN, userStateBuilder.build());
+            });
+
+            return envUsersStateMap;
         } catch (RuntimeException e) {
-            throw new UmsOperationException(String.format("Error during UMS operation: %s", e.getMessage()));
+            throw new UmsOperationException(String.format("Error during UMS operation: {}", e));
+        } finally {
+            UmsUsersStateProvider.requestId.remove();
         }
     }
 
-    private Map<String, UmsState> getEnvToUmsStateMap(
-        String accountId, String actorCrn, Set<String> environmentsFilter, List<User> users, List<MachineUser> machineUsers) {
-        UmsState.Builder umsStateBuilder = new UmsState.Builder();
+    private boolean isEnvironmentUser(String enviromentCrn, GetRightsResponse rightsResponse) {
 
-        // process each user and update environmentCRN -> UmsState map
-        Map<String, UmsState> envUmsStateMap = new HashMap<>();
-
-        environmentsFilter.stream().forEach(envCRN -> {
-            List<User> rightfulUsers = getUsersWithEnvironmentRights(actorCrn, envCRN, users);
-            umsStateBuilder.addUsers(rightfulUsers);
-
-            List<MachineUser> rightfulMachineUsers = getMachineUsersWithEnvironmentRights(actorCrn, envCRN, machineUsers);
-            umsStateBuilder.addMachineUsers(rightfulMachineUsers);
-
-            Map<String, List<Group>> userToGroupsMap = new HashMap<>();
-
-            // get all groups for identified users those having rights.
-            umsClient.getUsersToGroupsMap(userToGroupsMap, actorCrn, accountId, rightfulUsers, Optional.empty());
-            umsClient.getMachineUsersToGroupsMap(userToGroupsMap, actorCrn, accountId, rightfulMachineUsers, Optional.empty());
-
-            umsStateBuilder.addUserToGroupMap(userToGroupsMap);
-
-
-            envUmsStateMap.put(envCRN, umsStateBuilder.build());
-        });
-
-        return envUmsStateMap;
-    }
-
-    private List<User> getUsersWithEnvironmentRights(
-        String actorCrn, String envCRN, List<User> allUsers) {
-        List<User> rightfulUsers = new ArrayList<>();
-        // for all users, check right for the passed envCRN
-        for (User u : allUsers) {
-
-            if (umsClient.checkRight(actorCrn, u.getCrn(), environmentWrite, envCRN, Optional.empty())) {
-                // if (true) {
-                rightfulUsers.add(u);
-            }
-        }
-        return rightfulUsers;
-
-    }
-
-    private List<MachineUser> getMachineUsersWithEnvironmentRights(
-        String actorCrn, String envCRN, List<MachineUser> allMachineUsers) {
-
-        List<MachineUser> rightfulMachineUsers = new ArrayList<>();
-        // machine users
-        for (MachineUser machineUser : allMachineUsers) {
-
-            // Machine User can be a power user also
-            if (umsClient.checkRight(actorCrn, machineUser.getCrn(), environmentWrite, envCRN, Optional.empty())) {
-                // this is admin user having write access
-                rightfulMachineUsers.add(machineUser);
+        List<RoleAssignment> rolesAssignedList = rightsResponse.getRoleAssignmentList();
+        for (RoleAssignment roleAssigned : rolesAssignedList) {
+            // TODO: should come from IAM Roles and check against Role Object
+            if (roleAssigned.getRole().getCrn().contains("PowerUser") ||
+                roleAssigned.getRole().getCrn().contains("EnvironmentAdmin")) {
+                return true;
+                // admins are also users
             }
         }
 
-        return rightfulMachineUsers;
+        List<ResourceRoleAssignment> resourceRoleAssignedList = rightsResponse.getResourceRolesAssignmentList();
+        for (ResourceRoleAssignment resourceRoleAssigned : resourceRoleAssignedList) {
+            // TODO: should come from IAM Roles and check against Role Object
+            if (resourceRoleAssigned.getResourceRole().getCrn().contains("EnvironmentAdmin") ||
+                (resourceRoleAssigned.getResourceRole().getCrn().contains("EnvironmentUser"))) {
+                return true;
+            }
+        }
 
+        return false;
     }
+
+    private boolean isEnvironmentAdmin(String enviromentCrn, GetRightsResponse rightsResponse) {
+        List<RoleAssignment> rolesAssignedList = rightsResponse.getRoleAssignmentList();
+        for (RoleAssignment roleAssigned : rolesAssignedList) {
+            // TODO: should come from IAM Roles and check against Role Object
+            if (roleAssigned.getRole().getCrn().contains("PowerUser") ||
+                roleAssigned.getRole().getCrn().contains("EnvironmentAdmin")) {
+                return true;
+            }
+        }
+
+        List<ResourceRoleAssignment> resourceRoleAssignedList = rightsResponse.getResourceRolesAssignmentList();
+        for (ResourceRoleAssignment resourceRoleAssigned : resourceRoleAssignedList) {
+            // TODO: should come from IAM Roles and check against Role Object
+            if (resourceRoleAssigned.getResourceRole().getCrn().contains("EnvironmentAdmin")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void handleUser(UsersState.Builder userStateBuilder, Map<String, FmsGroup> crnToFmsGroup,
+            String actorCrn, String memberCrn, FmsUser fmsUser, String environmentCrn) {
+        Optional<String> requestIdOptional = Optional.ofNullable(requestId.get());
+
+        GetRightsResponse rightsResponse = umsClient.getRightsForUser(actorCrn, memberCrn, environmentCrn, requestIdOptional);
+        if (isEnvironmentUser(environmentCrn, rightsResponse)) {
+            userStateBuilder.addUser(fmsUser);
+            rightsResponse.getGroupCrnList().stream().forEach(gcrn -> {
+                userStateBuilder.addMemberToGroup(crnToFmsGroup.get(gcrn).getName(), fmsUser.getName());
+                userStateBuilder.addGroup(crnToFmsGroup.get(gcrn));
+            });
+            if (isEnvironmentAdmin(environmentCrn, rightsResponse)) {
+                userStateBuilder.addMemberToGroup("admins", fmsUser.getName());
+            }
+        }
+    }
+
+    private FmsUser umsUserToUser(User umsUser) {
+        FmsUser fmsUser = new FmsUser();
+        fmsUser.setName(umsUser.getWorkloadUsername());
+        fmsUser.setFirstName(getOrDefault(umsUser.getFirstName(), "None"));
+        fmsUser.setLastName(getOrDefault(umsUser.getLastName(), "None"));
+        return fmsUser;
+    }
+
+    private String getOrDefault(String value, String other) {
+        return (value == null || value.isBlank()) ? other : value;
+    }
+
+    private FmsUser umsMachineUserToUser(MachineUser umsMachineUser) {
+        FmsUser fmsUser = new FmsUser();
+        fmsUser.setName(umsMachineUser.getWorkloadUsername());
+        // TODO what should the appropriate first and last name be for machine users?
+        fmsUser.setFirstName("Machine");
+        fmsUser.setLastName("User");
+        return fmsUser;
+    }
+
+    private FmsGroup umsGroupToGroup(Group umsGroup) {
+        FmsGroup fmsGroup = new FmsGroup();
+        fmsGroup.setName(umsGroup.getGroupName());
+        return fmsGroup;
+    }
+
 }
